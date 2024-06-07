e84969d6734f7636caf67b8ccc949fd028d75bfd
Jared Neil A. Hortaleza <jaredneilhortaleza11@gmail.com>
2024-06-07 16:24:51 +0800

    added name fields for players

--- backend/src/models/game.js
+++ backend/src/models/game.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 const gameSchema = new mongoose.Schema({
   playerOne: {
+    name: { type: String, required: true },
     wins: { type: Number, required: true },
     losses: { type: Number, required: true },
     draws: { type: Number, required: true },
   },
   playerTwo: {
+    name: { type: String, required: true },
     wins: { type: Number, required: true },
     losses: { type: Number, required: true },
     draws: { type: Number, required: true },
